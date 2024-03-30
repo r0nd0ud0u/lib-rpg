b1da8285c93c581f1d6e8fcc04eaee59bf307b36
r0nd0ud0u <57643172+r0nd0ud0u@users.noreply.github.com>
2024-03-30 16:19:43 +0100

    Add gha (#2)
    * add gha
    
    * test
    
    * update gha
    
    * update utils.rs

---
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -0,0 +1,12 @@
+<!-- Please explain the changes you made -->
+
+<!--
+Please, make sure:
+- you have read the contributing guidelines:
+  nothing yet hehe
+- you have formatted the code using rustfmt:
+  https://github.com/rust-lang/rustfmt
+- you have checked that all tests pass, by running `cargo test --workspace`
+- you have updated the changelog (if needed):
+  nothing yet hehe
+-->
--- src/utils.rs
+++ src/utils.rs
@@ -1,17 +1,13 @@
 #[cxx::bridge]
-mod utils {
+mod utilsExtern {
     extern "Rust" {
         fn build_effect_name(raw_effect: &str, stats_name: &str) -> String;
     }
 }
 
-
-/**
- * @brief Utils::BuildEffectName
- * Returns the concatenation of effect str and stats str
- * If the effect str name is empty => only the stats str
- * If the stats str name is empty => only the effect str
- */
+/// * Returns the concatenation of effect str and stats str
+/// * If the effect str name is empty => only the stats str
+///* If the stats str name is empty => only the effect str
 pub fn build_effect_name(raw_effect: &str, stats_name: &str) -> String {
     let mut effect_name = "".to_string();
     if raw_effect.is_empty() && !stats_name.is_empty() {
@@ -29,7 +25,7 @@ mod tests {
     use crate::utils::build_effect_name;
 
     #[test]
-    fn build_effect_name_works() {
+    fn unit_build_effect_name_works() {
         // case args not empty
         let mut str = build_effect_name("effect", "stats");
         assert_eq!("stats-effect", str);
