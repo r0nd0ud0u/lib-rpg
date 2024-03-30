2937f7337b6567ab53548f4da99b2e74f1718197
rondoudou <marcpon@yahoo.fr>
2024-03-30 15:26:14 +0100

    add build_effect_name

--- src/utils.rs
+++ src/utils.rs
@@ -1,16 +1,46 @@
-use utils::Shared;
-
 #[cxx::bridge]
 mod utils {
-    #[derive(Clone)]
-    struct Shared {
-        v: u32,
-    }
     extern "Rust" {
-        fn rusty_cxxbridge_vector()-> Vec<Shared>;
+        fn build_effect_name(raw_effect: &str, stats_name: &str) -> String;
     }
 }
 
-pub fn rusty_cxxbridge_vector()-> Vec<Shared> {
-  [Shared{ v: 32 }].to_vec()
+
+/**
+ * @brief Utils::BuildEffectName
+ * Returns the concatenation of effect str and stats str
+ * If the effect str name is empty => only the stats str
+ * If the stats str name is empty => only the effect str
+ */
+pub fn build_effect_name(raw_effect: &str, stats_name: &str) -> String {
+    let mut effect_name = "".to_string();
+    if raw_effect.is_empty() && !stats_name.is_empty() {
+        effect_name = stats_name.to_string();
+    } else if !raw_effect.is_empty() && stats_name.is_empty() {
+        effect_name = raw_effect.to_string();
+    } else if !raw_effect.is_empty() && !stats_name.is_empty() {
+        effect_name = format!("{}-{}", stats_name, raw_effect);
+    }
+    effect_name.to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::utils::build_effect_name;
+
+    #[test]
+    fn build_effect_name_works() {
+        // case args not empty
+        let mut str = build_effect_name("effect", "stats");
+        assert_eq!("stats-effect", str);
+        // case effect str empty
+        str = build_effect_name("", "stats");
+        assert_eq!("stats", str);
+        // case stats empty
+        str = build_effect_name("effect", "");
+        assert_eq!("effect", str);
+        // case both args empty
+        str = build_effect_name("", "");
+        assert!(str.is_empty());
+    }
 }
