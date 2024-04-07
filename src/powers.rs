c3e6d54bf8b12da61d9b49d8794a07c145846c27
rondoudou <marcpon@yahoo.fr>
2024-04-07 13:58:35 +0200

    add power elara: is_damage_tx_heal_needy_ally

--- src/powers.rs
+++ src/powers.rs
@@ -3,6 +3,8 @@ pub mod ffi {
     pub struct Powers {
         /// Enables the critical of the next heal atk after a critical on damage atk
         pub is_crit_heal_after_crit: bool,
+        /// Enables the power to heal the most needy ally using damage tx of previous turn
+        pub is_damage_tx_heal_needy_ally: bool, 
     }
     extern "Rust" {
     }
