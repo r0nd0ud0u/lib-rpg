ee0bfe98b03adff25e5007a4bfe722fc66f65c04
r0nd0ud0u <marcpon@yahoo.fr>
2025-03-09 20:02:51 +0100

    init json decoding for character file

--- src/attack_type.rs
+++ src/attack_type.rs
@@ -1,4 +1,6 @@
-#[derive(Debug, Clone)]
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct AttackType {
     pub name: String,
     /* pub level: u8,
--- src/stats.rs
+++ src/stats.rs
@@ -1,12 +1,85 @@
-#[derive(Debug, Clone)]
-pub struct Stats {
-    pub deleteme: String,
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize, Default, Clone)]
+pub struct TxRx {
+    #[serde(rename = "Tx-rx-size")]
+    tx_rx_size: u64,
+    #[serde(rename = "Tx-rx-type")]
+    tx_rx_type: u64,
 }
 
-impl Default for Stats {
-    fn default() -> Self {
-        Stats {
-            deleteme: "".to_owned(),
-        }
-    }
+#[derive(Debug, Serialize, Deserialize, Default, Clone)]
+struct Attribute {
+    #[serde(rename = "Current")]
+    current: u32,
+    #[serde(default)]
+    current_raw: u32,
+    #[serde(rename = "Max")]
+    max: u32,
+    #[serde(default)]
+    max_raw: u32,
+    #[serde(default)]
+    buf_effect_value: u32,
+    #[serde(default)]
+    buf_effect_percent: u32,
+    #[serde(default)]
+    buf_equip_value: u32,
+    #[serde(default)]
+    buf_equip_percent: u32,
+}
+#[derive(Debug, Serialize, Deserialize, Default, Clone)]
+pub struct Stats {
+    #[serde(rename = "Aggro")]
+    aggro: Vec<Attribute>,
+
+    #[serde(default, rename = "Aggro rate")]
+    aggro_rate: Vec<Attribute>,
+
+    #[serde(default, rename = "Magic armor")]
+    magic_armor: Vec<Attribute>,
+
+    #[serde(default, rename = "Physical armor")]
+    physical_armor: Vec<Attribute>,
+
+    #[serde(default, rename = "Magic strength")]
+    magic_strength: Vec<Attribute>,
+
+    #[serde(default, rename = "Physical strength")]
+    physical_strength: Vec<Attribute>,
+
+    #[serde(default, rename = "HP")]
+    hp: Vec<Attribute>,
+
+    #[serde(default, rename = "Mana")]
+    mana: Vec<Attribute>,
+
+    #[serde(default, rename = "Vigor")]
+    vigor: Vec<Attribute>,
+
+    #[serde(default, rename = "Berseck")]
+    berseck: Vec<Attribute>,
+
+    #[serde(default, rename = "Berseck rate")]
+    berseck_rate: Vec<Attribute>,
+
+    #[serde(default, rename = "Speed")]
+    speed: Vec<Attribute>,
+
+    #[serde(default, rename = "Critical strike")]
+    critical_strike: Vec<Attribute>,
+
+    #[serde(default, rename = "Dodge")]
+    dodge: Vec<Attribute>,
+
+    #[serde(default, rename = "HP regeneration")]
+    regeneration_hp: Vec<Attribute>,
+
+    #[serde(default, rename = "Mana regeneration")]
+    regeneration_mana: Vec<Attribute>,
+
+    #[serde(default, rename = "Vigor regeneration")]
+    regeneration_vigor: Vec<Attribute>,
+
+    #[serde(default, rename = "Speed regeneration")]
+    regeneration_speed: Vec<Attribute>,
 }
