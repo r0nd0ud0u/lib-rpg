6d8cdb2b243ab47216c8f966997b8e12c10c0c44
r0nd0ud0u <marcpon@yahoo.fr>
2025-04-07 16:56:03 +0200

    update target test

--- src/testing_target.rs
+++ src/testing_target.rs
@@ -4,7 +4,7 @@ use crate::target::TargetInfo;
 
 pub fn build_target_boss_indiv() -> TargetInfo {
     TargetInfo {
-        name: "Boss1".to_owned(),
+        name: "Angmar".to_owned(),
         is_targeted: true,
         _is_boss: true,
         _is_reach_rand: false,
