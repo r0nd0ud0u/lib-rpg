539944e9f692bb6819f801002872e29ab164d8e0
r0nd0ud0u <marcpon@yahoo.fr>
2025-04-25 02:39:43 +0200

    add debug txt

--- src/players_manager.rs
+++ src/players_manager.rs
@@ -241,7 +241,7 @@ impl PlayerManager {
     pub fn update_current_player(&mut self, game_state: &GameState, name: &str) -> Result<()> {
         let c = self
             .get_mut_active_character(name)
-            .expect("no active character");
+            .expect(&format!("no active character: {}", name));
         self.current_player = c.clone();
 
         // update the shadow current player
