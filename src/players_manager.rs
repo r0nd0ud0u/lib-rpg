80bc88d39976a107a8b8f733917cc190b55de107
r0nd0ud0u <marcpon@yahoo.fr>
2026-03-01 15:57:36 +0100

    update data-manager  test

--- src/players_manager.rs
+++ src/players_manager.rs
@@ -2,7 +2,6 @@ use std::{collections::HashMap, path::Path};
 
 use anyhow::{Result, bail};
 use serde::{Deserialize, Serialize};
-use strum::IntoEnumIterator;
 
 use crate::{
     attack_type::AttackType,
@@ -10,7 +9,7 @@ use crate::{
     common::{
         all_target_const::{TARGET_ALL_HEROES, TARGET_ALLY, TARGET_ENNEMY, TARGET_HIMSELF},
         character_const::*,
-        paths_const::{OFFLINE_CHARACTERS, OFFLINE_LOOT_EQUIPMENT},
+        paths_const::OFFLINE_CHARACTERS,
         reach_const::{INDIVIDUAL, ZONE},
         stats_const::*,
     },
@@ -41,10 +40,6 @@ pub struct DodgeInfo {
 /// /// Should store all the relative data to all the playING characters
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct PlayerManager {
-    /// List of all playable heroes -> player
-    pub all_heroes: Vec<Character>,
-    /// List of all playable bosses -> computer
-    pub all_bosses: Vec<Character>,
     /// List of all selected heroes by players
     pub active_heroes: Vec<Character>,
     /// List of all selected bosses by computer
@@ -61,67 +56,13 @@ impl PlayerManager {
     /// if `default_active_characters` is true.
     /// Bosses are always active by default.
     /// `path` is the root path of the offline directory containing characters and equipments directories.
-    pub fn try_new<P: AsRef<Path>>(
-        path: P,
-        default_active_characters: bool,
-    ) -> Result<PlayerManager> {
-        let mut pl = PlayerManager {
-            all_heroes: Vec::new(),
-            all_bosses: Vec::new(),
+    pub fn new(equipment_table: HashMap<EquipmentJsonKey, Vec<Equipment>>) -> PlayerManager {
+        PlayerManager {
             active_heroes: Vec::new(),
             active_bosses: Vec::new(),
             current_player: Character::default(),
-            equipment_table: HashMap::new(),
-        };
-        // load all the equipments
-        // must be loaded before loading the characters
-        pl.load_all_equipments(&path)?;
-        // load all the characters
-        pl.load_all_characters(&path)?;
-        // By default, all the heroes are active
-        if default_active_characters {
-            pl.active_heroes = pl.all_heroes.clone();
+            equipment_table,
         }
-        // All the bosses are active
-        pl.active_bosses = pl.all_bosses.clone();
-        Ok(pl)
-    }
-
-    pub fn testing_pm() -> PlayerManager {
-        let mut pl = PlayerManager::try_new("tests/offlines", true).unwrap();
-        pl.current_player = pl.active_heroes[0].clone();
-        pl.load_all_equipments("./tests/offlines").unwrap();
-        pl
-    }
-
-    /// Load all the JSON files in a path `P` which corresponds to a directory.
-    /// Characters are inserted in Hero or Boss lists.
-    pub fn load_all_characters<P: AsRef<Path>>(&mut self, path: P) -> Result<()> {
-        if path.as_ref().as_os_str().is_empty() {
-            bail!("no root path")
-        }
-        let character_dir_path = path.as_ref().join(*OFFLINE_CHARACTERS);
-        match list_files_in_dir(&character_dir_path) {
-            Ok(list) => list.iter().for_each(|character_path| {
-                match Character::try_new_from_json(
-                    character_path,
-                    path.as_ref(),
-                    false,
-                    &self.equipment_table,
-                ) {
-                    Ok(c) => {
-                        if c.kind == CharacterType::Hero {
-                            self.all_heroes.push(c);
-                        } else {
-                            self.all_bosses.push(c);
-                        }
-                    }
-                    Err(e) => println!("{:?} cannot be decoded: {}", character_path, e),
-                }
-            }),
-            Err(e) => bail!("Files cannot be listed in {:#?}: {}", character_dir_path, e),
-        };
-        Ok(())
     }
 
     /// Characters are inserted in Hero or Boss lists.
@@ -159,34 +100,6 @@ impl PlayerManager {
         Ok(())
     }
 
-    pub fn load_all_equipments<P: AsRef<Path>>(&mut self, root_path: P) -> Result<()> {
-        if root_path.as_ref().as_os_str().is_empty() {
-            bail!("no root path")
-        }
-        let equipment_dir_path = root_path.as_ref().join(*OFFLINE_LOOT_EQUIPMENT);
-        // for each part of the equipment, load all the equipments and insert them in the equipment table
-        for part in EquipmentJsonKey::iter() {
-            let part_dir_path = equipment_dir_path.join(part.to_string());
-            match list_files_in_dir(&part_dir_path) {
-                Ok(list) => {
-                    list.iter().for_each(|equipment_path| {
-                        match Equipment::try_new_from_json(equipment_path) {
-                            Ok(e) => {
-                                self.equipment_table
-                                    .entry(part.clone())
-                                    .or_default()
-                                    .push(e);
-                            }
-                            Err(e) => println!("{:?} cannot be decoded: {}", equipment_path, e),
-                        }
-                    })
-                }
-                Err(e) => println!("Files cannot be listed in {:#?}: {}", part_dir_path, e),
-            };
-        }
-        Ok(())
-    }
-
     pub fn increment_counter_effect(&mut self) {
         for c in self.active_heroes.iter_mut() {
             c.increment_counter_effect();
@@ -212,9 +125,9 @@ impl PlayerManager {
     // TODO change swap remove see processCost
     pub fn apply_regen_stats(&mut self, kind: CharacterType) {
         let player_list = if kind == CharacterType::Hero {
-            &mut self.all_heroes
+            &mut self.active_heroes
         } else {
-            &mut self.all_bosses
+            &mut self.active_bosses
         };
         for pl in player_list {
             if pl.is_dead().unwrap_or(false) {
@@ -673,30 +586,26 @@ fn process_hot_or_dot(local_log: &mut Vec<String>, hot_and_dot: &mut i64, gae: &
 #[cfg(test)]
 mod tests {
     use crate::{
-        common::stats_const::*, equipment::EquipmentJsonKey, game_state::GameState,
-        players_manager::GameAtkEffects, testing_effect::*,
+        common::stats_const::*,
+        equipment::EquipmentJsonKey,
+        game_state::GameState,
+        players_manager::GameAtkEffects,
+        testing_all_characters::{self, testing_pm},
+        testing_effect::*,
     };
-
-    use super::PlayerManager;
     use strum::IntoEnumIterator;
 
     #[test]
     fn unit_try_new() {
-        let pl = PlayerManager::try_new("tests/offlines", true).unwrap();
-        assert_eq!(2, pl.active_heroes.len());
+        let pl = testing_all_characters::testing_pm();
 
-        assert!(PlayerManager::try_new("", false).is_err());
-
-        let pl = PlayerManager::try_new("tests/offlines", false).unwrap();
-        assert!(pl.active_heroes.is_empty());
-        assert_eq!(2, pl.all_heroes.len());
         // equipments
         assert_eq!(EquipmentJsonKey::iter().count(), pl.equipment_table.len());
     }
 
     #[test]
     fn unit_increment_counter_effect() {
-        let mut pl = PlayerManager::try_new("tests/offlines", true).unwrap();
+        let mut pl = testing_all_characters::testing_pm();
         pl.active_heroes[0].all_effects.push(GameAtkEffects {
             all_atk_effects: build_cooldown_effect(),
             ..Default::default()
@@ -715,81 +624,71 @@ mod tests {
 
     #[test]
     fn unit_reset_is_first_round() {
-        let mut pl = PlayerManager::try_new("tests/offlines", true).unwrap();
+        let mut pl = testing_all_characters::testing_pm();
         pl.reset_is_first_round();
-        assert!(pl.all_heroes[0].extended_character.is_first_round);
+        assert!(pl.active_heroes[0].extended_character.is_first_round);
     }
 
     #[test]
     fn unit_apply_regen_stats() {
-        let mut pl = PlayerManager::try_new("tests/offlines", true).unwrap();
-        let old_hp = pl.all_heroes[0].stats.all_stats[HP].current;
-        let hp_regen = pl.all_heroes[0].stats.all_stats[HP_REGEN].current;
-        let old_mana = pl.all_heroes[0].stats.all_stats[MANA].current;
-        let mana_regen = pl.all_heroes[0].stats.all_stats[MANA_REGEN].current;
-        let old_berseck = pl.all_heroes[0].stats.all_stats[BERSERK].current;
-        let berseck_regen = pl.all_heroes[0].stats.all_stats[BERSECK_RATE].current;
-        let old_vigor = pl.all_heroes[0].stats.all_stats[VIGOR].current;
-        let vigor_regen = pl.all_heroes[0].stats.all_stats[VIGOR_REGEN].current;
-        let old_speed = pl.all_heroes[0].stats.all_stats[SPEED].current;
-        let speed_regen = pl.all_heroes[0].stats.all_stats[SPEED_REGEN].current;
+        let mut pl = testing_all_characters::testing_pm();
+        let old_hp = pl.active_heroes[0].stats.all_stats[HP].current;
+        let hp_regen = pl.active_heroes[0].stats.all_stats[HP_REGEN].current;
+        let old_mana = pl.active_heroes[0].stats.all_stats[MANA].current;
+        let mana_regen = pl.active_heroes[0].stats.all_stats[MANA_REGEN].current;
+        let old_berseck = pl.active_heroes[0].stats.all_stats[BERSERK].current;
+        let berseck_regen = pl.active_heroes[0].stats.all_stats[BERSECK_RATE].current;
+        let old_vigor = pl.active_heroes[0].stats.all_stats[VIGOR].current;
+        let vigor_regen = pl.active_heroes[0].stats.all_stats[VIGOR_REGEN].current;
+        let old_speed = pl.active_heroes[0].stats.all_stats[SPEED].current;
+        let speed_regen = pl.active_heroes[0].stats.all_stats[SPEED_REGEN].current;
         pl.apply_regen_stats(crate::character::CharacterType::Hero);
         assert_eq!(
             old_hp + hp_regen,
-            pl.all_heroes[0].stats.all_stats[HP].current
+            pl.active_heroes[0].stats.all_stats[HP].current
         );
         assert_eq!(
             std::cmp::min(
                 old_mana + mana_regen,
-                pl.all_heroes[0].stats.all_stats[MANA].max
+                pl.active_heroes[0].stats.all_stats[MANA].max
             ),
-            pl.all_heroes[0].stats.all_stats[MANA].current
+            pl.active_heroes[0].stats.all_stats[MANA].current
         );
         assert_eq!(
             std::cmp::min(
                 old_berseck + berseck_regen,
-                pl.all_heroes[0].stats.all_stats[BERSERK].max
+                pl.active_heroes[0].stats.all_stats[BERSERK].max
             ),
-            pl.all_heroes[0].stats.all_stats[BERSERK].current
+            pl.active_heroes[0].stats.all_stats[BERSERK].current
         );
         assert_eq!(
             std::cmp::min(
                 old_vigor + vigor_regen,
-                pl.all_heroes[0].stats.all_stats[VIGOR].max
+                pl.active_heroes[0].stats.all_stats[VIGOR].max
             ),
-            pl.all_heroes[0].stats.all_stats[VIGOR].current
+            pl.active_heroes[0].stats.all_stats[VIGOR].current
         );
         assert_eq!(
             old_speed + speed_regen,
-            pl.all_heroes[0].stats.all_stats[SPEED].current
+            pl.active_heroes[0].stats.all_stats[SPEED].current
         );
 
-        let old_hp = pl.all_bosses[0].stats.all_stats[HP].current;
-        let hp_regen = pl.all_bosses[0].stats.all_stats[HP_REGEN].current;
+        let old_hp = pl.active_bosses[0].stats.all_stats[HP].current;
+        let hp_regen = pl.active_bosses[0].stats.all_stats[HP_REGEN].current;
         pl.apply_regen_stats(crate::character::CharacterType::Boss);
         // max is topped
         assert_eq!(
-            std::cmp::min(pl.all_bosses[0].stats.all_stats[HP].max, old_hp + hp_regen),
-            pl.all_bosses[0].stats.all_stats[HP].current
+            std::cmp::min(
+                pl.active_bosses[0].stats.all_stats[HP].max,
+                old_hp + hp_regen
+            ),
+            pl.active_bosses[0].stats.all_stats[HP].current
         );
-    }
-
-    #[test]
-    fn unit_load_all_characters() {
-        let mut pl = PlayerManager::default();
-        pl.load_all_characters("tests/offlines").unwrap();
-        assert_eq!(2, pl.all_heroes.len());
-    }
-
-    #[test]
-    fn unit_load_all_characters_err() {
-        let mut pl = PlayerManager::default();
-        assert!(pl.load_all_characters("").is_err());
     }
 
     #[test]
     fn unit_get_mut_active_character() {
-        let mut pl = PlayerManager::try_new("tests/offlines", true).unwrap();
+        let mut pl = testing_all_characters::testing_pm();
         assert!(pl.get_mut_active_character("test").is_some());
         assert!(pl.get_mut_active_character("test_boss1").is_some());
         assert!(pl.get_mut_active_character("unknown").is_none());
@@ -797,7 +696,7 @@ mod tests {
 
     #[test]
     fn unit_get_active_character() {
-        let pl = PlayerManager::try_new("tests/offlines", true).unwrap();
+        let pl = testing_all_characters::testing_pm();
         assert!(pl.get_active_character("test").is_some());
         assert!(pl.get_active_character("test_boss1").is_some());
         assert!(pl.get_active_character("unknown").is_none());
@@ -805,7 +704,7 @@ mod tests {
 
     #[test]
     fn unit_update_current_player() {
-        let mut pl = PlayerManager::testing_pm();
+        let mut pl = testing_all_characters::testing_pm();
         pl.get_mut_active_hero_character("test")
             .unwrap()
             .extended_character
@@ -825,7 +724,7 @@ mod tests {
 
     #[test]
     fn unit_process_hot_and_dot() {
-        let mut pl = PlayerManager::testing_pm();
+        let mut pl = testing_all_characters::testing_pm();
         // push default effect
         pl.current_player
             .all_effects
@@ -867,7 +766,7 @@ mod tests {
 
     #[test]
     fn unit_apply_hot_or_dot() {
-        let mut pl = PlayerManager::testing_pm();
+        let mut pl = testing_all_characters::testing_pm();
         let gs = GameState::default();
         pl.current_player.stats.all_stats[HP].current = 100;
         pl.current_player.stats.all_stats[HP].max = 100;
@@ -883,7 +782,7 @@ mod tests {
 
     #[test]
     fn unit_load_active_characters_from_saved_game() {
-        let mut pl = PlayerManager::testing_pm();
+        let mut pl = testing_all_characters::testing_pm();
         let result = pl.load_active_characters_from_saved_game("");
         assert!(result.is_err());
         let result = pl.load_active_characters_from_saved_game("unknown");
@@ -900,7 +799,7 @@ mod tests {
 
     #[test]
     fn unit_set_one_target() {
-        let mut pl = PlayerManager::testing_pm();
+        let mut pl = testing_all_characters::testing_pm();
         // simpleAtk is indiv launched by a boss
         pl.set_one_target("test_boss1", "SimpleAtk", "test");
         assert!(
@@ -947,7 +846,7 @@ mod tests {
 
     #[test]
     fn unit_set_targeted_characters() {
-        let mut pl = PlayerManager::testing_pm();
+        let mut pl = testing_pm();
         // hero is attacking
         // atk to ennemy - effect dmg indiv
         let test_ally_name = "test";
@@ -1222,12 +1121,5 @@ mod tests {
                 .expect("no hero")
                 .is_potential_target
         );
-    }
-
-    #[test]
-    fn unit_load_all_equipments() {
-        let mut pl = PlayerManager::default();
-        pl.load_all_equipments("./tests/offlines").unwrap();
-        assert_eq!(EquipmentJsonKey::iter().count(), pl.equipment_table.len());
     }
 }
